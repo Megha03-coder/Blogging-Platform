70fa24dd07e38e168c84a882f239129949bf1e11
megha03-coder <meghachauhan310@gmail.com>
2025-06-17 15:07:10 +0530

    project setup with setting backend

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Auth failed" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = (req, res, next) => {
+  if (req.user?.role !== 'admin') {
+    return res.status(403).json({ message: "Access denied. Admin only." });
+  }
+  next();
+};
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/authController');
+
+router.post('/register', register);
+router.post('/login', login);
+const authMiddleware = require('../middleware/authMiddleware');
+const User = require('../models/User');
+
+router.get('/me', authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+module.exports = router;
